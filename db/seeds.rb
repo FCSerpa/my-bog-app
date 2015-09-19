ea4d0c7d9c20ad12fe1e90ec6d7ac72d5798b83f
Francis Serpa <fcserpa@gmail.com>
2015-09-18 18:29:12 -0700

    filled seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Creature.create({name: "Constrictor Snake", description: "This large snake has a thick, muscular body and dull green scales decorated with dirty brown stripes."})
+Creature.create({name: "Hydra", description: "Multiple angry snake-like heads rise from the sleek, serpentine body of this terrifying monster."})
+Creature.create({name: "Leech Swarm", description: "A mass of wriggling black creatures each the size of a man's finger stirs the stagnant water before enveloping its victim."})
+Creature.create({name: "Gray Ooze", description: "A seemingly mundane puddle, patch of moist stone, or glistening rock is suddenly revealed to be more as a terrible pseudopod lashes out.")}
+Creature.create({name: "Green Hag", description: "Knots of dark, moldering hair spill over the features of this sickly, thin, green-skinned crone.")}
+Creature.create({name: "Ochre Jelly", description: "This yellow-orange amoeboid creature slithers across the ground, pseudopods grasping ahead of its slow approach.")}
+Creature.create({name: "7 Stirges", description: "This insectoid creature has two pairs of bat wings, a tangle of thin legs, and a needle-sharp proboscis.")}
+Creature.create({name: "4 Crocodiles", description: "This reptile lunges out of the placid water with shocking speed. Its jaw gapes open in a roar, its powerful tail lashing behind.")}
+Creature.create({name: "7 Giant Frogs", description: "This creature looks like a normal frog, with moist, mottled, blackish-green skin, but grown to truly monstrous size.")}
+Creature.create({name: "7 Goblin Dogs", description: "This mangy canine's face has the same flat nose, beady eyes, and protruding teeth as a rat grown grotesquely large.")}
+Creature.create({name: "3 Harpies", description: "Save for the tattered wings and taloned feet, this creature resembles a feral woman with a wild look about her.")}
+Creature.create({name: "7 Lizardfolk", description: "This reptilian humanoid has green scales, a short and toothy snout, and a thick alligator-like tail.")}
+Creature.create({name: "7 Boggards", description: "Bulbous eyes glare atop this creature's decidedly toad-like head. A multitude of warts and bumps decorate its greenish skin.")}
+Creature.create({name: "Chuul", description: "This lobster-like creature has a thick armored shell. A pair of tiny eyes gleams above a mouth full of writhing tentacles.")}
+Creature.create({name: "Dracolisk", description: "This six-legged dragon flaps its massive wings as it lowers its head to glare with hideous glowing eyes.")}
+Creature.create({name: "7 Giant Leeches", description: "A parasite ballooned to monstrous proportions undulates in the muck, its circular maw a spiral of teeth.")}
+Creature.create({name: "Young Black Dragon", description: "Hissing green acid drips from the fanged maw of this black-scaled, horned dragon."})
+Creature.create({name: "Giant Slug", description: "This massive, gray-brown slug leaves a viscous trail of slime in its wake as a long rasping tongue tastes the air for prey.")}
+Creature.create({name: "7 Shambling Mounds", description: "A mass of tangled vines and dripping slime rises on two trunk-like legs, reeking of rot and freshly turned earth.")}
+Creature.create({name: "Spirit Naga", description: "Yellowed, venom-dripping fangs fill the human-like mouth of this sinister serpentine monstrosity.")}
+Creature.create({name: "4 Will-O'-Wisps", description: "This faintly glowing ball of light bobs gently in the air, the nebulous image of what might be a skull visible somewhere in its depths.")}
+Creature.create({name: "Dire Crocodile", description: "This reptilian behemoth, a crocodile of monstrous proportions, is large enough to swallow a horse in one tremendous bite.")}
+Creature.create({name: "Giant Flytrap", description: "This towering plant is a mass of vines and barbs. Several stalks are horribly mobile, each ending in a set of green, toothy jaws.")}
+Creature.create({name: "Froghemoth", description: "This immense, three-eyed frog-like creature rears up on powerful hind legs. In place of arms, four huge tentacles thrash and writhe.")}
