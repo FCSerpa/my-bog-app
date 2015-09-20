4ade551b1cd1661868afc3daf44133cefa53eaed
Francis Serpa <fcserpa@gmail.com>
2015-09-20 15:56:34 -0700

    made creatures show route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   
   get '/creatures', to: "creatures#index"
   get '/creatures/new', to: 'creatures#new'
+  get '/creatures/:id', to: 'creatures#show'
   post "/creatures", to: "creatures#create"
 
 end
